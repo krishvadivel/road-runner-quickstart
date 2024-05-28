8225cf54acaeb72af51d28d18e0039a6fb9a3dc1
krishvadivel <142186360+krishvadivel@users.noreply.github.com>
2024-05-28 14:27:37 -0500

    Update MotorPractice.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorPractice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorPractice.java
@@ -1,24 +1,27 @@
 package org.firstinspires.ftc.teamcode;
 
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
-
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+
 
 import java.util.concurrent.TimeUnit;
 
-
-public class MotorPractice {
+@TeleOp(group = "ZTest")
+public class MotorPractice extends LinearOpMode{
     public void runOpMode() throws InterruptedException {
         DcMotor motor = hardwareMap.dcMotor.get("motor1");
+        DcMotor motor6 = hardwareMap.dcMotor.get("motor6");
         waitForStart();
         if (isStopRequested()) return;
         while (opModeIsActive()) {
             if (gamepad1.a) {
-                motor.setPower(1);
+                motor.setPower(0.4);
+                motor6.setPower(0.4);
                 TimeUnit.MILLISECONDS.sleep(350);
             } else if (gamepad1.b) {
                 motor.setPower(0);
+                motor6.setPower(0.1);
                 TimeUnit.MILLISECONDS.sleep(350);
             }
         }
